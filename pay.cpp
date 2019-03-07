3c847cc057806dc30029ac84a5bfa538457fde8b
zacharyhaag <32313367+zacharyhaag@users.noreply.github.com>
2019-03-07 12:08:43 -0800

    Create pay.cpp

---
+++ pay.cpp
@@ -0,0 +1,13 @@
+#include "person.cpp"
+#include <vector>
+#include <iostream> 
+using namespace std;
+
+int main()
+{
+cout << "hey" << endl;
+const int NUM_EMPLOYEES = 16; 
+vector<int> 
+
+return 0;
+}
